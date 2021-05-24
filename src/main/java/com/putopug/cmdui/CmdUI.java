7634c5548dcb3bd52f1e510cea332f0d3f45ee5b
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-05-24 17:26:45 +0530

    Got java.io.File to make a slots.json

--- src/main/java/com/putopug/cmdui/CmdUI.java
+++ src/main/java/com/putopug/cmdui/CmdUI.java
@@ -19,6 +19,8 @@ public final class CmdUI extends JavaPlugin {
         getCommand("cmdui").setExecutor(new UICmd());
         this.getServer().getPluginManager().registerEvents(new Listener(),this);
         invStuff();
+        this.saveDefaultConfig();
+        DataEngine.init();
     }
 
     @Override
