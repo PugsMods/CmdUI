972c388457ff755109d81acadcfa9bebcb36f4fb
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-05-26 22:55:16 +0530

    Commands should work now

--- src/main/java/com/putopug/cmdui/Listener.java
+++ src/main/java/com/putopug/cmdui/Listener.java
@@ -1,5 +1,8 @@
 package com.putopug.cmdui;
 
+import com.putopug.cmdui.config.DeJsonizer;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -13,13 +16,19 @@ public class Listener implements org.bukkit.event.Listener {
         if (!event.getInventory().equals(Inventory.inventory)) return;
         if (event.getCurrentItem() == null) return;
         if (event.getCurrentItem().getItemMeta() == null) return;
-        if (event.getCurrentItem().getItemMeta().getDisplayName() == null) return;
 
         event.setCancelled(true);
         Player player = (Player) event.getWhoClicked();
 
-        if (event.getSlot() == 13) {
-            player.teleport(event.getWhoClicked().getWorld().getSpawnLocation());
+        for (Object x: DeJsonizer.slts.getSlots().keySet()){
+            if(event.getSlot() == Integer.parseInt(x.toString().replaceAll("slot_",""))){
+                if(!(DeJsonizer.slts.getSlots().get(x.toString()).getCommand().equals("${cmdui.utils.tp_to_world_spawn}"))){
+                    player.teleport(event.getWhoClicked().getWorld().getSpawnLocation());
+                    return;
+                }else{
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(),DeJsonizer.slts.getSlots().get(x.toString()).getCommand().replaceAll("\\$\\{player_name}",player.getDisplayName()));
+                }
+            }
         }
     }
 }
